89eb4d509318f9064585cc1e72b5e41f78737d0f
Abdelaziz Soliman <abdelaziz.soliman89@gmail.com>
2024-11-30 11:34:51 +0200

    Implement cart list

--- user/ecom/src/route/AppRoute.js
+++ user/ecom/src/route/AppRoute.js
@@ -9,6 +9,7 @@ import RefundPage from '../pages/RefundPage'
 import ProductDetailsPage from '../pages/ProductDetailsPage'
 import NotificationPage from '../pages/NotificationPage'
 import FavouritePage from '../pages/FavouritePage'
+import CartPage from '../pages/CartPage'
 
 
 
@@ -26,6 +27,7 @@ class AppRoute extends Component {
                     <Route path="/productdetails" element={<ProductDetailsPage />} />
                     <Route path="/notification" element={<NotificationPage />} />
                     <Route path="/favourite" element={<FavouritePage />} />
+                    <Route path="/cart" element={<CartPage />} />
                 </Routes>
             </Fragment>
         )
