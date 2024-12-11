a32f41a55fdc1d3f00c04fe67cec4d7746a4c1d5
Abdelaziz Soliman <abdelaziz.soliman89@gmail.com>
2024-12-11 11:10:53 +0200

    Create category and subcategory tables and make JSON array

--- admin/adminecom/routes/api.php
+++ admin/adminecom/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\VisitorController;
 use App\Http\Controllers\Admin\ContactController;
 use App\Http\Controllers\Admin\SiteInfoController;
+use App\Http\Controllers\Admin\CategoryController;
 
 
 // Route::get('/user', function (Request $request) {
@@ -16,3 +17,4 @@ Route::get('/getvisitor',[VisitorController::class, 'GetVisitorDetails']);
 Route::post('/postcontact',[ContactController::class, 'PostContactDetails']);
 Route::post('/postcontact',[ContactController::class, 'PostContactDetails']);
 Route::get('/allsiteinfo',[SiteInfoController::class, 'AllSiteinfo']);
+Route::get('/allcategory',[CategoryController::class, 'Allcategory']);
