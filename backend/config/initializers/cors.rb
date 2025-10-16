a434642bce206d4e299321e394b79336869b6f3a
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2025-10-16 03:28:28 +0300

    Update environment

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -2,7 +2,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("FRONTEND_ORIGIN", "http://localhost:3001")
+    origins(
+      ENV.fetch("FRONTEND_ORIGIN", "http://localhost:3001"),
+      "http://127.0.0.1:3001",
+      "https://pwa-ecommerce-psi.vercel.app"
+    )
     resource "*", headers: :any, methods: %i[get post put patch delete options head], credentials: true
   end
 end
